e25ddcd2966ee81e9f00e4820cd763d2233b0f0c
yksl <yukselkurtuldu9@gmail.com>
2024-03-26 13:51:36 +0300

    logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
+import NbaLogo from "../assets/nba-logo.png";
+import "../App.css"
+
 const Header = ({ text, onSearchChange }) => {
   return (
     <div className="Header">
+      <img src={NbaLogo} alt="logo" />
       <h1>{text}</h1>
       <input 
         type="text" 
@@ -14,52 +18,6 @@ const Header = ({ text, onSearchChange }) => {
 export default Header;
 
 
-
-// import React, { useState } from 'react';
-// import { data } from '../helper/data';
-// import NbaLogo from "../assets/nba-logo.png";
-// import "../App.css";
-
-// const Header = ({ text }) => {
-//   const [searchTerm, setSearchTerm] = useState('');
-//   const [filteredPlayers, setFilteredPlayers] = useState([]);
-
-//   const handleSearch = (event) => {
-//     const value = event.target.value.toLowerCase();
-//     setSearchTerm(value);
-
-//     const filtered = data.filter(player =>
-//       player.name.toLowerCase().includes(value)
-//     );
-
-//     setFilteredPlayers(filtered);
-//   };
-
-//   return (
-//     <div className="Header">
-//       <img src={NbaLogo} alt="NBA Logo" />
-//       <h1>{text}</h1>
-//       <input 
-//         type="search" 
-//         placeholder='Search Player...'
-//         value={searchTerm}
-//         onChange={handleSearch}
-//       />
-//       <div>
-//         {filteredPlayers.map((player, index) => (
-//           <div className='container'>
-//           <div className='card' key={index}>
-//             <img className="card-img" src={player.img} alt={player.name}/>
-//             <p>{player.name}</p>
-//           </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Header;
 
 
 
